f913a31613b5c43d460b3f7f99e03f4cfbbd1dcc
Koder09 <68038925+CodeDude404@users.noreply.github.com>
2022-03-16 21:02:57 +0000

    fixed indentation bug

--- main.py
+++ main.py
@@ -120,12 +120,12 @@ async def leave(ctx):
 
 @bot.command(aliases=['paly', 'queue', 'que'])
 async def play(ctx, file = None):
-    guild = ctx.guild
-    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+	guild = ctx.guild
+	voice_client: discord.VoiceClient = 		discord.utils.get(bot.voice_clients, guild=guild)
 	if file != None:
 		audio_source = discord.FFmpegPCMAudio("assets/"+file)
 		if not voice_client.is_playing():
-    		voice_client.play(audio_source, after=None)
+			voice_client.play(audio_source, after=None)
 		
 # ==========================================================
 #Reply to DM's
