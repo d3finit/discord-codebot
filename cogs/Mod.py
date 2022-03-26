c23e8d9817847c2704080ce0686334813cc014df
Koder09 <68038925+CodeDude404@users.noreply.github.com>
2022-03-26 21:25:30 +0000

    moved the moderation cog to be in a separate file

---
+++ cogs/Mod.py
@@ -0,0 +1,145 @@
+import discord
+import os
+from discord.ext import commands
+
+# ==========================================================
+# Moderator Cog
+# ==========================================================
+class Moderation(commands.Cog):
+	def __init__(self, client):
+		self.client = client
+		self._last_member = None
+
+	# Events
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print("Moderation Cog loaded.")
+
+	# Commands
+	# Mute command - Only users with the "Mod" role can use this
+	@commands.command()
+	@commands.has_role("Mod")
+	async def mute(self, ctx, member: discord.Member):
+		"""Mute users."""
+		mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+		await member.add_roles(mutedRole)
+	# await member.send(f"You have been muted on the server: {ctx.guild.name}")
+
+		if member.nick == "None":
+			embed = discord.Embed(title=f"Muted {member.name}", description=f"Sucessfully muted user {member.mention}",colour=discord.Colour.gold())
+		
+		else:
+			embed = discord.Embed(title=f"Muted {member.nick}", description=f"Sucessfully muted user {member.mention}",colour=discord.Colour.gold())	
+			
+		await ctx.send(embed=embed)
+		print(f"Muted user {member.mention}")
+
+
+	
+	
+	# Unmute command - Only users with the "Mod" role can use this	
+	@commands.command()
+	@commands.has_role("Mod")
+	async def unmute(self, ctx, member: discord.Member):
+		"""Unmute users."""
+		mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+		await member.remove_roles(mutedRole)
+	# await member.send(f"You have been unmuted on the server: {ctx.guild.name}")
+
+		if member.nick == "None":
+			embed = discord.Embed(title=f"Unmuted {member.name}", description=f"Sucessfully unmuted user {member.mention}",colour=discord.Colour.gold())
+		
+		else:
+			embed = discord.Embed(title=f"Unmuted {member.nick}", description=f"Sucessfully unmuted user {member.mention}",colour=discord.Colour.gold())	
+		
+		await ctx.send(embed=embed)
+		print(f"Unmuted user {member.mention}")
+
+
+
+
+		
+	
+	# Lock command - Only users with the "Mod" role can use this
+	@commands.command()
+	@commands.has_role("Mod")
+	async def lock(self, ctx, channel: discord.TextChannel):
+		"""Locks channels."""
+		channel = channel or ctx.channel
+		overwrite = channel.overwrites_for(ctx.guild.default_role)
+		overwrite.send_messages = False
+		await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+		await ctx.send('Channel locked.')
+
+		
+	
+	# Unlock command - Only users with the "Mod" role can use this	
+	@commands.command()
+	@commands.has_role("Mod")
+	async def unlock(self, ctx, channel: discord.TextChannel):
+		"""Unlocks channels."""
+		channel = channel or ctx.channel
+		overwrite = channel.overwrites_for(ctx.guild.default_role)
+		overwrite.send_messages = Null
+		await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+		await ctx.send('Channel unlocked.')
+
+	
+	# Banword command - Only users with the "Admin" role can use this	
+	@commands.command()
+	@commands.has_role("Admin")
+	async def banword(self, ctx, word):
+		"""Bans words from being said on the server."""
+		gname = ctx.guild.name.replace(" ", "")
+		os.system(f"mkdir conf/server/{gname}")
+		# print("Made folder")
+		os.system(f"touch conf/server/{gname}/bannedwords.txt")
+		# print("Made file")
+
+		f = open(f"conf/server/{gname}/bannedwords.txt", "a")
+		f.write(f"\n{word}")
+		f.close()
+
+		embed = discord.Embed(title=f"Added {word} to the banned words list.", description=f"Sucessfully banned {word}. Unban it with $unbanword {word}",colour=discord.Colour.red())	
+		
+		await ctx.send(embed=embed)
+
+	
+	
+	
+	# Unbanword command - Only users with the "Admin" role can use this
+	@commands.command()
+	@commands.has_role("Admin")
+	async def unbanword(self, ctx, word):
+		"""Unbans words from being said on the server."""
+		gname = ctx.guild.name.replace(" ", "")
+		# Make sure this is the valid path to your file
+		file = f"conf/server/{gname}/bannedwords.txt"
+		remove = word
+
+		# Read in the file
+		with open(file, "r") as f:
+			filedata = f.read()
+
+		# Replace the target string
+		filedata = filedata.replace(remove, "")
+		filedata = filedata.replace("\n","")
+
+		# Write the file out again
+		with open(file, "w") as f:
+			f.write(filedata)
+
+		embed = discord.Embed(title=f"Removed {word} from the banned words list.", description=f"Sucessfully unbanned {word}. Ban it with $banword {word}",colour=discord.Colour.red())	
+		
+		await ctx.send(embed=embed)
+
+
+
+
+
+
+
+# Setup command - runs when the cog is loaded.
+def setup(client):
+	client.add_cog(Moderation(client))
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from discord.ext import commands
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 client = discord.Client()
+
+
 with open("TOKEN.txt") as f:
 	TOKEN = f.read()
 if TOKEN == "":
@@ -46,138 +48,6 @@ async def on_ready():
 
 
 	
-# ==========================================================
-# Mod Features
-# ==========================================================
-
-
-async def mute(ctx, member: discord.Member):
-		"""Mutes users"""
-		mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-
-		await member.add_roles(mutedRole)
-	# await member.send(f"You have been muted on the server: {ctx.guild.name}")
-
-		if member.nick == "None":
-			embed = discord.Embed(title=f"Muted {member.name}", description=f"Sucessfully muted user {member.mention}",colour=discord.Colour.gold())
-		
-		else:
-			embed = discord.Embed(title=f"Muted {member.nick}", description=f"Sucessfully muted user {member.mention}",colour=discord.Colour.gold())	
-			
-		await ctx.send(embed=embed)
-		print(f"Muted user {member.mention}")
-
-async def unmute(ctx, member: discord.Member):
-		mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-		"""Unmutes users"""
-		await member.remove_roles(mutedRole)
-	# await member.send(f"You have been unmuted on the server: {ctx.guild.name}")
-
-		if member.nick == "None":
-			embed = discord.Embed(title=f"Unmuted {member.name}", description=f"Sucessfully unmuted user {member.mention}",colour=discord.Colour.gold())
-		
-		else:
-			embed = discord.Embed(title=f"Unmuted {member.nick}", description=f"Sucessfully unmuted user {member.mention}",colour=discord.Colour.gold())	
-		
-		await ctx.send(embed=embed)
-		print(f"Unmuted user {member.mention}")
-
-
-
-async def lock(ctx, channel : discord.TextChannel=None):
-	channel = channel or ctx.channel
-	overwrite = channel.overwrites_for(ctx.guild.default_role)
-	overwrite.send_messages = False
-	await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
-	await ctx.send('Channel locked.')
-
-
-async def unlock(ctx, channel : discord.TextChannel=None):
-	channel = channel or ctx.channel
-	overwrite = channel.overwrites_for(ctx.guild.default_role)
-	overwrite.send_messages = Null
-	await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
-	await ctx.send('Channel unlocked.')
-
-async def banword(ctx, word):
-	gname = ctx.guild.name.replace(" ", "")
-	os.system(f"mkdir conf/server/{gname}")
-	# print("Made folder")
-	os.system(f"touch conf/server/{gname}/bannedwords.txt")
-	# print("Made file")
-
-
-	f = open(f"conf/server/{gname}/bannedwords.txt", "a")
-	f.write(f"\n{word}")
-	f.close()
-
-	embed = discord.Embed(title=f"Added {word} to the banned words list.", description=f"Sucessfully banned {word}. Unban it with $unbanword {word}",colour=discord.Colour.red())	
-		
-	await ctx.send(embed=embed)
-
-async def unbanword(ctx, word):
-	gname = ctx.guild.name.replace(" ", "")
-	# Make sure this is the valid path to your file
-	file = f"conf/server/{gname}/bannedwords.txt"
-	remove = word
-
-	# Read in the file
-	with open(file, "r") as f:
-		filedata = f.read()
-
-	# Replace the target string
-	filedata = filedata.replace(remove, "")
-	filedata = filedata.replace("\n","")
-
-	# Write the file out again
-	with open(file, "w") as f:
-		f.write(filedata)
-
-	embed = discord.Embed(title=f"Removed {word} from the banned words list.", description=f"Sucessfully unbanned {word}. Ban it with $banword {word}",colour=discord.Colour.red())	
-		
-	await ctx.send(embed=embed)
-
-class Moderation(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-		self._last_member = None
-
-
-	@commands.command()
-	@commands.has_role("Mod")
-	async def mute(self, ctx, user: discord.Member):
-		"""Mute users."""
-		await mute(ctx, user) # uses the mute function
-
-	@commands.command()
-	@commands.has_role("Mod")
-	async def unmute(self, ctx, user: discord.Member):
-		"""Unmute users."""
-		await unmute(ctx, user) # uses the unmute function
-
-	@commands.command()
-	@commands.has_role("Mod")
-	async def lock(self, ctx, channel: discord.TextChannel):
-		"""Locks channels."""
-		await lock(ctx, channel) # uses the lock function
-		
-	@commands.command()
-	@commands.has_role("Mod")
-	async def unlock(self, ctx, channel: discord.TextChannel):
-		"""Unlocks channels."""
-		await unlock(ctx, channel) # uses the unlock function
-		
-	@commands.command()
-	@commands.has_role("Admin")
-	async def banword(self, ctx, word):
-		"""Bans words from being said on the server."""
-		await banword(ctx, word) # uses the banword function
-
-	@commands.command()
-	@commands.has_role("Admin")
-	async def unbanword(self, ctx, word):
-		"""Unbans words from being said on the server."""
-		await unbanword(ctx, word) # uses the unbanword function
 
 
 # Banned words listener
@@ -199,14 +69,6 @@ async def msgevent(message):
 				if linevalid:	
 					print("Banned word detected, removing...")
 					await message.delete()
-
-					
-bot.add_cog(Moderation(bot))
-
-
-
-
-
 
 # purge command
 @bot.command(name='purge', help='Deletes x messages',aliases=['clear','nuke','wipe'])
@@ -346,5 +208,11 @@ async def msgevent(message):
 				f = open(f'conf/user/{message.author}.json', "w")
 				f.write(open("conf/user/default.json", "r").read())
 				f.close()
+
+
+
+for filename in os.listdir('./cogs'):
+	if filename.endswith('.py'):
+		bot.load_extension(f"cogs.{filename[:-3]}")
 		
 bot.run(TOKEN)
